fb636698f63f7a9443636bf0363d5fe332b88bb7
Greg M <44076044+gmireles210@users.noreply.github.com>
2019-11-18 22:59:00 -0800

    updated assets

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ function ProductAll(name, imgUrl) {
 ProductAll.arrayImg = [];
 
 ProductAll.arrayImg.push(new ProductAll('Bag', 'lab/assets/bag.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Banana', 'lab/assetsbanana.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Bathroom', 'lab/assetsbathroom.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Boots', 'lab/assetsboots.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Breakfast', 'lab/assetsbreakfast.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Bubble Gum', 'lab/assetsbubblegum.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Chair', 'lab/assetschair.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Cthulhu', 'lab/assetsctulhu.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Dog Duck', 'lab/assetsdog-duck.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Dragon', 'lab/assetsdragon.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Pen', 'lab/assetspen.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Pet Sweep', 'lab/assetspet-sweep.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Scissors', 'lab/assetsscissors.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Shark', 'lab/assetsshark.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Sweep', 'lab/assetssweep.png'));
-ProductAll.arrayImg.push(new ProductAll('Tauntaun', 'lab/assetstauntaun.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Unicorn', 'lab/assetsunicorn.jpg'));
-ProductAll.arrayImg.push(new ProductAll('USB', 'lab/assetsusb.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Water Can', 'lab/assetswater-can.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Wine Glass', 'lab/assetswine-glass.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Banana', 'lab/assets/banana.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Bathroom', 'lab/assets/bathroom.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Boots', 'lab/assets/boots.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Breakfast', 'lab/assets/breakfast.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Bubble Gum', 'lab/assets/bubblegum.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Chair', 'lab/assets/chair.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Cthulhu', 'lab/assets/ctulhu.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Dog Duck', 'lab/assets/dog-duck.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Dragon', 'lab/assets/dragon.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Pen', 'lab/assets/pen.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Pet Sweep', 'lab/assets/pet-sweep.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Scissors', 'lab/assets/scissors.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Shark', 'lab/assets/shark.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Sweep', 'lab/assets/sweep.png'));
+ProductAll.arrayImg.push(new ProductAll('Tauntaun', 'lab/assets/tauntaun.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Unicorn', 'lab/assets/unicorn.jpg'));
+ProductAll.arrayImg.push(new ProductAll('USB', 'lab/assets/usb.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Water Can', 'lab/assets/water-can.jpg'));
+ProductAll.arrayImg.push(new ProductAll('Wine Glass', 'lab/assets/wine-glass.jpg'));
 // ProductAllNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 ProductAll.prototype.fetchRandomImages = function() {
