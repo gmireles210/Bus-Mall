1a4d0b45227972b47f303e93429856cf9c3e80c0
Greg M <44076044+gmireles210@users.noreply.github.com>
2019-11-19 07:55:25 -0800

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ ProductAll.arrayImg.push(new ProductAll('Unicorn', 'lab/assets/unicorn.jpg'));
 ProductAll.arrayImg.push(new ProductAll('USB', 'lab/assets/usb.jpg'));
 ProductAll.arrayImg.push(new ProductAll('Water Can', 'lab/assets/water-can.jpg'));
 ProductAll.arrayImg.push(new ProductAll('Wine Glass', 'lab/assets/wine-glass.jpg'));
-// ProductAllNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 ProductAll.prototype.fetchRandomImages = function() {
   var prodThreeImages = [];
@@ -38,10 +37,10 @@ ProductAll.prototype.fetchRandomImages = function() {
   for(var i = 0; i < 3; i++) {
     prodThreeImages.push(ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())]);
 
-    // var imageOne = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
-    // var imgageTwo = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
-    // var imageThree = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
-  }
+    /*var imageOne = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
+    var imgageTwo = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
+    var imageThree = ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())];
+  }*/
 
   prodThreeImages.map(function(ProductAll){
     return ProductAll.name;
