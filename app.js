20b3959fa9ccf09cb4d923232b13618d0ba9e9e0
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-19 20:56:32 -0800

    updated functions

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var firstProduct = null;
 var secondProduct = null;
 var thirdProduct = null;
 
-function product(name, imgUrl) {
+function Product(name, imgUrl) {
   this.name = name;
   this.imgUrl = imgUrl;
   this.clickCtr = 0;
@@ -20,7 +20,7 @@ function product(name, imgUrl) {
   Product.allProducts.push(this);
 }
 
-product.arrayImg = [];
+Product.arrayImg = [];
 
 // Helper function to display document elements
 function addElement(tag, container, text) {
@@ -40,31 +40,31 @@ function checkDuplicate(objName, arr) {
   return match;
 }
 
-new product('Bag', 'lab/assets/bag.jpg'));
-new product('Banana', 'lab/assets/banana.jpg'));
-new product('Bathroom', 'lab/assets/bathroom.jpg'));
-new product('Boots', 'lab/assets/boots.jpg'));
-new product('Breakfast', 'lab/assets/breakfast.jpg'));
-new product('Bubble Gum', 'lab/assets/bubblegum.jpg'));
-new product('Chair', 'lab/assets/chair.jpg'));
-new product('Cthulhu', 'lab/assets/cthulhu.jpg'));
-new product('Dog Duck', 'lab/assets/dog-duck.jpg'));
-new product('Dragon', 'lab/assets/dragon.jpg'));
-new product('Pen', 'lab/assets/pen.jpg'));
-new product('Pet Sweep', 'lab/assets/pet-sweep.jpg'));
-new product('Scissors', 'lab/assets/scissors.jpg'));
-new product('Shark', 'lab/assets/shark.jpg'));
-new product('Sweep', 'lab/assets/sweep.jpg'));
-new product('Tauntaun', 'lab/assets/tauntaun.jpg'));
-new product('Unicorn', 'lab/assets/unicorn.jpg'));
-new product('USB', 'lab/assets/usb.jpg'));
-new product('Water Can', 'lab/assets/water-can.jpg'));
-new product('Wine Glass', 'lab/assets/wine-glass.jpg'));
+new Product('Bag', 'lab/assets/bag.jpg');
+new Product('Banana', 'lab/assets/banana.jpg');
+new Product('Bathroom', 'lab/assets/bathroom.jpg');
+new Product('Boots', 'lab/assets/boots.jpg');
+new Product('Breakfast', 'lab/assets/breakfast.jpg');
+new Product('Bubble Gum', 'lab/assets/bubblegum.jpg');
+new Product('Chair', 'lab/assets/chair.jpg');
+new Product('Cthulhu', 'lab/assets/cthulhu.jpg');
+new Product('Dog Duck', 'lab/assets/dog-duck.jpg');
+new Product('Dragon', 'lab/assets/dragon.jpg');
+new Product('Pen', 'lab/assets/pen.jpg');
+new Product('Pet Sweep', 'lab/assets/pet-sweep.jpg');
+new Product('Scissors', 'lab/assets/scissors.jpg');
+new Product('Shark', 'lab/assets/shark.jpg');
+new Product('Sweep', 'lab/assets/sweep.jpg');
+new Product('Tauntaun', 'lab/assets/tauntaun.jpg');
+new Product('Unicorn', 'lab/assets/unicorn.jpg');
+new Product('USB', 'lab/assets/usb.jpg');
+new Product('Water Can', 'lab/assets/water-can.jpg');
+new Product('Wine Glass', 'lab/assets/wine-glass.jpg');
 
 // Used Stackoverflow for assistance with below equation //
 function fetchThreeProd() {
   var previousSet = [];
-  if (currentFirstProduct !== null) {
+  if (firstProduct !== null) {
     previousSet.push(firstProduct.name);
     previousSet.push(secondProduct.name);
     previousSet.push(thirdProduct.name);
@@ -109,9 +109,9 @@ function clicker(event) {
     thirdProduct.shownCtr++;
 
     if(id === 'prodOneImage') {
-      currentFirstProduct.clickCtr++;
+      firstProduct.clickCtr++;
     } else if(id === 'prodTwoImage') {
-      currentSecondProduct.clickCtr++;
+      secondProduct.clickCtr++;
     } else if(id === 'prodThreeImage') {
       thirdProduct.clickCtr++;
     }
