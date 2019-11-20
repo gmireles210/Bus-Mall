5f4c6c76f76045890351fc524a3c730742d67f32
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-11-19 20:49:31 -0800

    updated app, index, css

--- app.js
+++ app.js
@@ -1,60 +1,211 @@
-
 'use strict';
 //Global functions
 var allProdImages = document.getElementById('allProdImages');
 
-function ProductAll(name, imgUrl) {
-  this.name = name;
-  this.imgUrl = imgUrl;
-}
-ProductAll.arrayImg = [];
-
-ProductAll.arrayImg.push(new ProductAll('Bag', 'lab/assets/bag.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Banana', 'lab/assets/banana.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Bathroom', 'lab/assets/bathroom.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Boots', 'lab/assets/boots.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Breakfast', 'lab/assets/breakfast.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Bubble Gum', 'lab/assets/bubblegum.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Chair', 'lab/assets/chair.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Cthulhu', 'lab/assets/cthulhu.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Dog Duck', 'lab/assets/dog-duck.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Dragon', 'lab/assets/dragon.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Pen', 'lab/assets/pen.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Pet Sweep', 'lab/assets/pet-sweep.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Scissors', 'lab/assets/scissors.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Shark', 'lab/assets/shark.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Sweep', 'lab/assets/sweep.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Tauntaun', 'lab/assets/tauntaun.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Unicorn', 'lab/assets/unicorn.jpg'));
-ProductAll.arrayImg.push(new ProductAll('USB', 'lab/assets/usb.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Water Can', 'lab/assets/water-can.jpg'));
-ProductAll.arrayImg.push(new ProductAll('Wine Glass', 'lab/assets/wine-glass.jpg'));
-
-// Used Stackoverflow for assistance with below equation //
-ProductAll.prototype.fetchRandomImages = function() {
-  var prodThreeImages = [];
-  var prodDuplex = false;
-  for(var i = 0; i < 3; i++) {
-    prodThreeImages.push(ProductAll.arrayImg[Math.floor(ProductAll.arrayImg.length * Math.random())]);
-  }
-
-  prodThreeImages.map(function(ProductAll){
-    return ProductAll.name;
-  }).forEach(function(element, index, array){
-    if(array.indexOf(element) !== index) {
-      prodDuplex = true;
-      if(prodDuplex) {
-        ProductAll.prototype.fetchRandomImages();
-      }
-    }
-  });
-  return prodThreeImages;
-}
-
 var prodOneImage = document.getElementById('prodOneImage');
 var prodTwoImage = document.getElementById('prodTwoImage');
 var prodThreeImage = document.getElementById('prodThreeImage');
+var resultList = document.getElementById('resultList');
+var ctx = document.getElementById('chartResults').getContext('2d');
 
-prodOneImage.setAttribute('src', ProductAll.prototype.fetchRandomImages()[0].imgUrl);
-prodTwoImage.setAttribute('src', ProductAll.prototype.fetchRandomImages()[1].imgUrl);
-prodThreeImage.setAttribute('src', ProductAll.prototype.fetchRandomImages()[2].imgUrl);
+var firstProduct = null;
+var secondProduct = null;
+var thirdProduct = null;
+
+function product(name, imgUrl) {
+  this.name = name;
+  this.imgUrl = imgUrl;
+  this.clickCtr = 0;
+  this.shownCtr = 0;
+  Product.allProducts.push(this);
+}
+
+product.arrayImg = [];
+
+// Helper function to display document elements
+function addElement(tag, container, text) {
+  var element = document.createElement(tag);
+  container.appendChild(element);
+  element.textContent = text;
+  return element;
+}
+
+function checkDuplicate(objName, arr) {
+  var match = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (objName === arr[i].name) {
+      match++;
+    }
+  }
+  return match;
+}
+
+new product('Bag', 'lab/assets/bag.jpg'));
+new product('Banana', 'lab/assets/banana.jpg'));
+new product('Bathroom', 'lab/assets/bathroom.jpg'));
+new product('Boots', 'lab/assets/boots.jpg'));
+new product('Breakfast', 'lab/assets/breakfast.jpg'));
+new product('Bubble Gum', 'lab/assets/bubblegum.jpg'));
+new product('Chair', 'lab/assets/chair.jpg'));
+new product('Cthulhu', 'lab/assets/cthulhu.jpg'));
+new product('Dog Duck', 'lab/assets/dog-duck.jpg'));
+new product('Dragon', 'lab/assets/dragon.jpg'));
+new product('Pen', 'lab/assets/pen.jpg'));
+new product('Pet Sweep', 'lab/assets/pet-sweep.jpg'));
+new product('Scissors', 'lab/assets/scissors.jpg'));
+new product('Shark', 'lab/assets/shark.jpg'));
+new product('Sweep', 'lab/assets/sweep.jpg'));
+new product('Tauntaun', 'lab/assets/tauntaun.jpg'));
+new product('Unicorn', 'lab/assets/unicorn.jpg'));
+new product('USB', 'lab/assets/usb.jpg'));
+new product('Water Can', 'lab/assets/water-can.jpg'));
+new product('Wine Glass', 'lab/assets/wine-glass.jpg'));
+
+// Used Stackoverflow for assistance with below equation //
+function fetchThreeProd() {
+  var previousSet = [];
+  if (currentFirstProduct !== null) {
+    previousSet.push(firstProduct.name);
+    previousSet.push(secondProduct.name);
+    previousSet.push(thirdProduct.name);
+  }
+
+  var disp_Images = [];
+  while (disp_Images.length < 3) {
+    var radnoIndex = Math.floor(Math.random() * Product.allProducts.length);
+    // Check Ducplicate on three displayed img
+    if (checkDuplicate(Product.allProducts[radnoIndex].name, disp_Images) === 0) {
+      // Check immediate previous set
+      if (previousSet.includes(Product.allProducts[radnoIndex].name) === false) {
+        disp_Images.push(Product.allProducts[radnoIndex]);
+      }
+    }
+  }
+  prodOneImage.src = disp_Images[0].imgURL;
+  prodTwoImage.src = disp_Images[1].imgURL;
+  prodThreeImage.src = disp_Images[2].imgURL;
+
+  firstProduct = disp_Images[0];
+  secondProduct = disp_Images[1];
+  thirdProduct = disp_Images[2];
+}
+
+fetchThreeProd();
+
+function totalResult() {
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    addElement('li', resultList, Product.allProducts[i].name + ' had ' + Product.allProducts[i].clickCtr + ' votes and was shown ' + Product.allProducts[i].shownCtr + ' times');
+  }
+}
+
+var counter = 0;
+function clicker(event) {
+  if (counter < 25) {
+    var id = event.target.id;
+
+    // Counter goes up every time the img is shown
+    firstProduct.shownCtr++;
+    secondProduct.shownCtr++;
+    thirdProduct.shownCtr++;
+
+    if(id === 'prodOneImage') {
+      currentFirstProduct.clickCtr++;
+    } else if(id === 'prodTwoImage') {
+      currentSecondProduct.clickCtr++;
+    } else if(id === 'prodThreeImage') {
+      thirdProduct.clickCtr++;
+    }
+
+    fetchThreeProd();
+  }
+  counter++;
+
+  if(counter === 25) {
+    alert('Done!');
+    createChart();
+    totalResult();
+  }
+}
+
+allProdImages.addEventListener('click', clicker);
+
+// Create result chart
+function createChart() {
+  var label = [];
+  var v_Data = [];
+  var displayedData = [];
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    label.push(Product.allProducts[i].name);
+    v_Data.push(Product.allProducts[i].clickCtr);
+    displayedData.push(Product.allProducts[i].shownCtr);
+  }
+  // eslint-disable-next-line no-undef
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: label,
+      datasets: [{
+        label: 'Number of Votes',
+        data: v_Data,
+        backgroundColor: [
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+        ],
+        borderColor: [
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            stepSize: 1,
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+};
